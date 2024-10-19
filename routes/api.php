9a5ed07b23686d0e9eb4b5b17652230550d82a41
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2024-10-19 13:14:25 +0700

    hapus flutter

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/check-connection', function () {
-    return response()->json([
-        'status' => 'success',
-        'message' => 'Laravel and Flutter are connected!'
-    ]);
-});
